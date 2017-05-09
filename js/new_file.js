4404990c85d050201d89d5c6808f1b64f9e8edab
TF999 <1540458559@qq.com>
2017-05-09 11:20:20 +0800

    first commit

---
+++ js/new_file.js
@@ -0,0 +1,30 @@
+
+ window.onload=function(){
+        var btm=document.getElementById("btm");
+        var xod=document.getElementById("ccc");
+        var dox=btm.getElementsByTagName("li")
+        dox[0].onmouseover=function(){
+            xod.src="img/149169972080548316.jpg"
+        }
+        dox[1].onmouseover=function(){
+            xod.src="img/149103927343504887.jpg"
+        }
+        dox[2].onmouseover=function(){
+            xod.src="img/149156647477327867.jpg"
+        }
+        dox[3].onmouseover=function(){
+            xod.src="img/149157481163528542.jpg"
+        }
+        dox[4].onmouseover=function(){
+           	xod.src="img/149157516289142885.jpg"
+        }
+        dox[5].onmouseover=function(){
+           	xod.src="img/149157530546221194.jpg"
+        }
+        dox[6].onmouseover=function(){
+           	xod.src="img/149155144535434417.jpg"
+        }
+        dox[7].onmouseover=function(){
+           	xod.src="img/149157620625801387.png"
+        }
+}
